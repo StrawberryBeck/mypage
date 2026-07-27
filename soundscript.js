168ba73d97e41c68eb5858e0c52540df9a17915c
StrawberryBeck <beckhadyk@gmail.com>
2026-07-27 23:00:50 +0200

    finaaaaaaal

--- soundscript.js
+++ soundscript.js
@@ -7,8 +7,6 @@ const sound = new Audio("../soundfolder/bubblesound.mp3");
 
 image.addEventListener("click", () => {
     sound.currentTime = 0;
-    sound.play().catch(error => {
-        console.error("The sound could not be played:", error);
-    });
+    sound.play()
 });
 
